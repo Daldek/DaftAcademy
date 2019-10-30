6e14f995a4501849871720c106f82a397666e626
Daldek <38719982+Daldek@users.noreply.github.com>
2019-10-30 17:16:34 +0100

    Reorganized
    Functions were moved to other python file

---
+++ Funkcje.py
@@ -0,0 +1,64 @@
+import re
+
+def elements2list(li):
+    return list(map(lambda el:[el], li))
+
+def list2tuple(li):
+    return (*li, )
+
+def ship_info(information_sought, list_of_ships):
+    # Odczytywanie informacji o statku
+    # Grupa #0 to cale wyrazenie
+    # Grupa #1 to id statku
+    # Grupa #2 to jego nazwa
+    # Grupa #3 to klasa do ktorej nalezy
+    extracted_info = []
+    for ship in list_of_ships:
+        ship_pattern = re.compile(r'(\d+): (\w+-?\w+) \((\w+ ?-?\w+?)\)')
+        ship_matches = ship_pattern.finditer(ship)
+        for ship_match in ship_matches:
+            extracted_info.append(ship_match.group(information_sought))
+    return extracted_info
+
+def container_info(information_sought, list_of_containers):
+    # Odczytywanie informacji o kontenerze
+    # Grupa #0 to cale wyrazenie
+    # Grupa #1 to kraj pochodzenia kontenera
+    # Grupa #2 to kraj docelowy
+    # Grupa #3 to numer kontenera
+    # Grupa #4 to waga
+    # Grupa #5 to typ ładunku (A0-Z9)
+    # Grupa #6 to nazwa firmy nadajacej kontener
+    # Grupa #7 to kraj pochodzenia firmy
+    # Grupa #8 to koszt w USD
+    extracted_info = []
+    for container in list_of_containers:
+        container_pattern = re.compile(
+            r'([A-Z][A-Z])-([A-Z][A-Z])-(\d+)/(\d\d\d\d)/(\w\w)@(\w+)\.'
+            '([a-z][a-z])/(\d+)')
+        container_matches = container_pattern.finditer(container)
+        for container_match in container_matches:
+            extracted_info.append(container_match.group(information_sought))
+    return extracted_info
+
+def containers2country(destination, list_of_containers):
+    containers_info_list = container_info(2, list_of_containers)
+    number_of_containers = 0
+    for container_destination in containers_info_list:
+        if container_destination == destination:
+            number_of_containers += 1
+        else:
+            pass
+    return number_of_containers
+
+def cargo_type_index(type, list_of_containers):
+    containers_info_list = container_info(5, list_of_containers)
+    index_list = []
+    container_index = 0
+    for cargo_type in containers_info_list:
+        if cargo_type == type:
+            index_list.append(container_index)
+        else:
+            pass
+        container_index += 1
+    return index_list
--- Kontenery.py
+++ Kontenery.py
@@ -1,29 +1,23 @@
 import pandas as pd
-import re
+from re import compile
 import math
+from Funkcje import *
 
 # df to skrot od Data Frame
-# df = pd.read_csv('dane.csv', sep=';', header=0, dtype=str)
-df = pd.read_csv('C:\\Users\\PLPD00293\\Desktop\\DaftAcademy\\dane.csv', sep=';', header=0, dtype=str)
+df = pd.read_csv('dane.csv', sep=';', header=0, dtype=str)
 
 ShipsCSV = df.columns  # pobiera statki z pliku CSV
 Ships = []  # zawiera zestawienie wszystkich statkow z naglowkow kolumn
-containers = []  # zbiera informacje o kontenerach na analizowanym statku
 filtered_df = []  # usuwa puste rekordy ze zmiennej 'df'
+containers = []  # zbiera informacje o kontenerach na analizowanym statku
 all_containers = []  # zawiera wszystkie kontenery
-ships_info = []  # ship_info() zwraca tutaj informacje o statkach
-containers_info = []  # container_info() zwraca tutaj informacje o kontenerach
 x1_list = []  # lista zawierajaca spis wszystkich konenerow X1
-containers_indexes = []  # indeksy kontenerow X1 na liscie all_containers
 x1_weight_list = []  # lista zawierajaca spis ciezarow wszystkich kontenerow X1
 ship_id = 0  # robocze id nadane aktualnie analizowanemu statkowi
-container_id = 0  # robocze id nadane aktualnie analizowanemu kontenerowi
-destination_jp = 0  # Zliczenie kontenerow plynacych do Japonii
-destination_country = 'JP'
 
 # Przeniesienie statkow do nowej listy
-for shipCSV in ShipsCSV:
-    Ships.append(shipCSV)
+for ship in ShipsCSV:
+    Ships.append(ship)
 
 while ship_id != len(Ships):
     filtered_df = df[df[Ships[ship_id]].notnull()]
@@ -37,102 +31,18 @@ while ship_id != len(Ships):
     containers = []
     ship_id += 1
 
-def convert2list(li):
-    return list(map(lambda el:[el], li))
-
-def convert2tuple(list):
-    return (*list, )
-
-def ship_info(ship_group_number):
-    # Odczytywanie informacji o statku
-    # Grupa #0 to cale wyrazenie
-    # Grupa #1 to id statku
-    # Grupa #2 to jego nazwa
-    # Grupa #3 to klasa do ktorej nalezy
-    for ship in Ships:
-        ship_pattern = re.compile(r'(\d+): (\w+-?\w+) \((\w+ ?-?\w+?)\)')
-        ship_matches = ship_pattern.finditer(ship)
-        for ship_match in ship_matches:
-            if ship_group_number == 0:
-                ships_info.append(ship_match.group(0))
-            elif ship_group_number == 1:
-                ships_info.append(ship_match.group(1))
-            elif ship_group_number == 2:
-                ships_info.append(ship_match.group(2))
-            elif ship_group_number == 3:
-                ships_info.append(ship_match.group(3))
-    return ships_info
-
-def container_info(container_group_number):
-    # Odczytywanie informacji o kontenerze
-    # Grupa #0 to cale wyrazenie
-    # Grupa #1 to kraj pochodzenia kontenera
-    # Grupa #2 to kraj docelowy
-    # Grupa #3 to numer kontenera
-    # Grupa #4 to waga
-    # Grupa #5 to typ ładunku (A0-Z9)
-    # Grupa #6 to nazwa firmy nadajacej kontener
-    # Grupa #7 to kraj pochodzenia firmy
-    # Grupa #8 to koszt w USD
-    for container_name in all_containers:
-        container_pattern = re.compile(r'([A-Z][A-Z])-([A-Z][A-Z])-(\d+)/(\d\d\d\d)/(\w\w)@(\w+)\.([a-z][a-z])/(\d+)')
-        container_matches = container_pattern.finditer(container_name)
-        for container_match in container_matches:
-            if container_group_number == 0:
-                containers_info.append(container_match.group(0))
-            elif container_group_number == 1:
-                containers_info.append(container_match.group(1))
-            elif container_group_number == 2:
-                containers_info.append(container_match.group(2))
-            elif container_group_number == 3:
-                containers_info.append(container_match.group(3))
-            elif container_group_number == 4:
-                containers_info.append(container_match.group(4))
-            elif container_group_number == 5:
-                containers_info.append(container_match.group(5))
-            elif container_group_number == 6:
-                containers_info.append(container_match.group(6))
-            elif container_group_number == 7:
-                containers_info.append(container_match.group(7))
-            elif container_group_number == 8:
-                containers_info.append(container_match.group(8))
-    return containers_info
-
-def containers2jp(number_of_containers, destination):
-    container_info(2)
-    for container_destination in containers_info:
-        if container_destination == destination:
-            number_of_containers += 1
-        else:
-            pass
-    return number_of_containers
-
-def x1_indexes(x1_containers_indexes):
-    container_info(5)
-    container_index = 0
-    for cargo_type in containers_info:
-        if cargo_type == "X1":
-            x1_containers_indexes.append(container_index)
-        else:
-            pass
-        container_index += 1
-    return x1_containers_indexes
-
 # Konwersja listy do slownika
-ship_info(3)
+ships_info = ship_info(3, Ships)
 ships_classes = dict.fromkeys(ships_info, 0)
 print(ships_classes)
-ships_info.clear()
 
 # Zliczenie kontenerow plynacych do Japonii
-destination_jp = containers2jp(destination_jp, destination_country)
+destination_jp = containers2country("JP", all_containers)
 print("Desination JP:", destination_jp)
-containers_info.clear()
 
 # Znalezienie indeksow ladunkow typu X1 na liscie all_containers
-containers_indexes = x1_indexes(containers_indexes)
+containers_indexes = cargo_type_index("X1", all_containers)
 print("Number of X1 containers:", len(containers_indexes))
-containers_info.clear()
 
 # Kopiowanie kontenerow z ladunkiem X1 na nowa liste
 for element in containers_indexes:
@@ -156,11 +66,11 @@ for ship_name in Ships:
     total_rows = df[ship_name].count()
     total_rows_list.append(total_rows)
 
-ship_class_list = ship_info(3)
+ship_class_list = ship_info(3, Ships)
 
 load_cargo_index = 0
 
-ship_class_list = convert2list(ship_class_list)
+ship_class_list = elements2list(ship_class_list)
 
 for each_ship in ship_class_list:
     each_ship.append(total_rows_list[load_cargo_index])
